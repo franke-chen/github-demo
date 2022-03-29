3ec63f6d582067c819c5d976be327cce5dc2b085
Franke.Chen <franke.chen@cn.abb.com>
2022-03-29 19:45:07 +0800

    fix lint, update actions

--- home-web/src/app/dashboard/setting/setting.component.spec.ts
+++ home-web/src/app/dashboard/setting/setting.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import{ HttpClientTestingModule } from "@angular/common/http/testing";
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { SettingComponent } from './setting.component';
 import { RouterTestingModule } from '@angular/router/testing';
--- home-web/src/app/signup/signup/signup.component.spec.ts
+++ home-web/src/app/signup/signup/signup.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import{ HttpClientTestingModule } from "@angular/common/http/testing";
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { SignupComponent } from './signup.component';
 import { RouterTestingModule } from '@angular/router/testing';
